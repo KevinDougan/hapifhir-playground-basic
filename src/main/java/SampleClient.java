12dbbec571015508d7964b317c61308e15434d92
Kevin Dougan <2100158+KevinDougan@users.noreply.github.com>
2020-08-13 00:57:01 -0400

    issue3 - Basic Task - Print out a sorted list of Patient First and Last Names and Birthdates.

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -1,8 +1,12 @@
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
-import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.Patient;
+
+import org.hl7.fhir.r4.model.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class SampleClient {
 
@@ -21,7 +25,33 @@ public class SampleClient {
                 .returnBundle(Bundle.class)
                 .execute();
 
-
+        // Process the results and produce the output
+        if (response.getEntry() != null && !response.getEntry().isEmpty()) {
+            List<String> namesList = new ArrayList<>();
+            Iterator<Bundle.BundleEntryComponent> iter = response.getEntry().listIterator();
+            while (iter.hasNext()) {
+                Bundle.BundleEntryComponent entry = iter.next();
+                Resource resource = entry.getResource();
+                if (resource instanceof Patient) {
+                    Patient patient = ((Patient)resource);
+                    String birthDateString = "";
+                    if (patient.getBirthDateElement() != null &&
+                            patient.getBirthDateElement().asStringValue() != null) {
+                        birthDateString = patient.getBirthDateElement().asStringValue();
+                    }
+                    List<HumanName> humanNames = patient.getName();
+                    // Write out all the Names for this Person
+                    for (HumanName humanName : humanNames) {
+                        String firstName = "";
+                        if (humanName.getGiven() != null && !humanName.getGiven().isEmpty()) {
+                            // Use the first name found since any additional ones are middle names
+                            firstName = humanName.getGiven().listIterator().next().toString();
+                        }
+                        namesList.add(firstName + " " + humanName.getFamily() + " " + birthDateString);
+                    }
+                }
+            }
+            namesList.stream().sorted().forEach(n -> System.out.println(n));
+        }
     }
-
 }
